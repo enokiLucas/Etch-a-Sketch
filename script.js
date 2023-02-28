d4c3dad3d380de4025b7581a6e855f9f925a28ed
Lucas Enoki <enoki.lucas@gmail.com>
2023-02-28 12:51:48 -0300

    makeDiv() now add a class to the div

--- script.js
+++ script.js
@@ -2,18 +2,17 @@
 //Add a red border to the grid and blue border to each div
 
 //make one div
-const makeDiv = () => {
+const makeDiv = (text, className) => {
 	const div = document.createElement('div');
 	const bodyGridContainer = document.querySelector('body .grid-container');
 	bodyGridContainer.appendChild(div);
-	div.textContent ='Hello';
+	div.className = className;
+	div.textContent = text;
 }
 
-makeDiv();
-/*
+
 for(let i = 0; i<16; i++) {
 	for(let j = 0; j<16; j++) {
-		makeDiv();
+		makeDiv((i+1) +'x'+ (j+1), 'grid-item');
 	}
 }
-*/
