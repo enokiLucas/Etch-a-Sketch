279259af4c9a4f35daf74a45758ed56081e7c9a9
Lucas Enoki <enoki.lucas@gmail.com>
2023-02-28 12:30:44 -0300

    create makeDiv() that creates a div inside .grid-container

--- script.js
+++ script.js
@@ -4,12 +4,16 @@
 //make one div
 const makeDiv = () => {
 	const div = document.createElement('div');
-	const body = document.querySelector('body .grid-container');
-	body.appendChild(div);
+	const bodyGridContainer = document.querySelector('body .grid-container');
+	bodyGridContainer.appendChild(div);
+	div.textContent ='Hello';
 }
 
+makeDiv();
+/*
 for(let i = 0; i<16; i++) {
 	for(let j = 0; j<16; j++) {
 		makeDiv();
 	}
 }
+*/
