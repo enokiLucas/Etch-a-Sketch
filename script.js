86bbaf873895edf359b0a6d905675f440b67a7f2
Lucas Enoki <enoki.lucas@gmail.com>
2023-02-28 22:02:50 -0300

    Change the background color when the mouse passes over it

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //make a grid of 16x16 div
 //Add a red border to the grid and blue border to each div
 
-//make one div
+//make one div and give it a class
 const makeDiv = (text, className) => {
 	const div = document.createElement('div');
 	const bodyGridContainer = document.querySelector('body .grid-container');
@@ -10,9 +10,19 @@ const makeDiv = (text, className) => {
 	div.textContent = text;
 }
 
-
+//call makeDiv() 16x16 times. And give it the class 'grid-item'
 for(let i = 0; i<16; i++) {
 	for(let j = 0; j<16; j++) {
 		makeDiv((i+1) +'x'+ (j+1), 'grid-item');
 	}
 }
+
+//Create an nodeList with all the .grid-item elements
+const gridItem = document.querySelectorAll('.grid-item');
+
+//Change the background color of all .grid-items
+gridItem.forEach((item) => {
+	item.addEventListener('mouseenter', () => {
+		item.style.backgroundColor = 'gray';
+	})
+})
