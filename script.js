53a805f2953c3edfe5fcd29aff3f8af8151c1e9d
Lucas Enoki <enoki.lucas@gmail.com>
2023-03-04 15:26:43 -0300

    modified makeDiv()

--- script.js
+++ script.js
@@ -2,18 +2,17 @@
 //Add a red border to the grid and blue border to each div
 
 //make one div and give it a class
-const makeDiv = (text, className) => {
+const makeDiv = () => {
 	const div = document.createElement('div');
 	const bodyGridContainer = document.querySelector('body .grid-container');
 	bodyGridContainer.appendChild(div);
-	div.className = className;
-	div.textContent = text;
+	div.className = 'grid-item';
 }
 
 //call makeDiv() 16x16 times. And give it the class 'grid-item'
 for(let i = 0; i<5; i++) {
 	for(let j = 0; j<5; j++) {
-		makeDiv((i+1) +'x'+ (j+1), 'grid-item');
+		makeDiv();
 	}
 }
 
