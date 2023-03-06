2e1f4191744f4c09ca5ccf2849b819ef332fd214
Lucas Enoki <enoki.lucas@gmail.com>
2023-03-06 16:49:04 -0300

    delete branch

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-//make a grid of 16x16 div
-//Add a red border to the grid and blue border to each div
+//Default grid 16x16
+const gridContainer = document.querySelector('.grid-container');
+gridContainer.style.gridTemplateColumns = 'repeat(16, 1fr)';
 
 //make one div and give it a class
 const makeDiv = () => {
@@ -9,13 +10,26 @@ const makeDiv = () => {
 	div.className = 'grid-item';
 }
 
-//call makeDiv() 16x16 times. And give it the class 'grid-item'
-for(let i = 0; i<55; i++) {
-	for(let j = 0; j<55; j++) {
+for(let i = 0; i<16; i++) {
+	for(let j = 0; j<16; j++) {
 		makeDiv();
 	}
 }
 
+//define the number of columns  the new grid will have
+const numberOfColumns = (lengthOfGrid) => {
+	let x = 'repeat(' + lengthOfGrid + ', 1fr)';
+	gridContainer.style.gridTemplateColumns = x;
+}
+
+//call makeDiv() 16x16 times. And give it the class 'grid-item'
+const makeAllDiv = (lengthOfGrid) => {
+	for(let i = 0; i<lengthOfGrid; i++) {
+		for(let j = 0; j<lengthOfGrid; j++) {
+			makeDiv();
+		}
+	}
+}
 //Create an nodeList with all the .grid-item elements
 const gridItem = document.querySelectorAll('.grid-item');
 
@@ -38,31 +52,14 @@ const buttonOK = document.querySelector('#button-ok');
 buttonOK.addEventListener('click', btnOk = () => {
 
 	//Get the height and length from the dialog.
-	let newHeight = document.getElementById('height').value;
 	let newLength = document.getElementById('length').value;
 	//Convert the string into number.
-	newHeight = parseInt(newHeight);
 	newLength = parseInt(newLength);
 
-	const newArray = [newHeight, newLength];
-	return newArray;
+	numberOfColumns(newLength);
+	makeAllDiv(newLength);
 })
 
-let heightA;
-let lengthA;
 
-//setter functions
-const getNewHeight = () => {
-	let array = [];
-	array = btnOk();
-	heightA = array[0];
-}
-
-const getNewLength = () => {
-	let array = [];
-	array = btnOk();
-	lengthA = array[1];
-}
-
-const gridContainer = document.querySelector('.grid-container');
-gridContainer.style.gridTemplateColumns = 'repeat(16, 1fr)';
+//const gridContainer = document.querySelector('.grid-container');
+//gridContainer.style.gridTemplateColumns = 'repeat(16, 1fr)';
